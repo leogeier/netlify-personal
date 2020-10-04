90292fdc35b9fe5cbd2dcd45799a92aed0a059a9
leogeier <leonard.geier@hotmail.com>
2020-10-05 00:59:05 +0200

    leaderboard for impact juggling

---
+++ leaderboard/fetch_highscores.js
@@ -0,0 +1,20 @@
+function create_td(content) {
+  const td_tag = document.createElement("td");
+  const text = document.createTextNode(content);
+  td_tag.appendChild(text);
+  return td_tag;
+}
+
+window.onload = async () => {
+  const scores = await ( await fetch("http://stuff.leonardgeier.com/highscore?count=10") ).json();
+
+  const table_tag = document.getElementById("leaderboard-body");
+  for (let i = 0; i < scores.length; i++) {
+    const {name, score} = scores[i];
+    const tag = document.createElement("tr");
+    tag.appendChild(create_td(i + 1));
+    tag.appendChild(create_td(name));
+    tag.appendChild(create_td(score));
+    table_tag.appendChild(tag);
+  }
+};
