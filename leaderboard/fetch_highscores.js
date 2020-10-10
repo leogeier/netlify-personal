9924d96e49ff9f95c9dc8fdf79332523b1292cf3
leogeier <leonard.geier@hotmail.com>
2020-10-10 22:41:23 +0200

    change leaderboard style and show all scores

--- leaderboard/fetch_highscores.js
+++ leaderboard/fetch_highscores.js
@@ -6,13 +6,13 @@ function create_td(content) {
 }
 
 window.onload = async () => {
-  const scores = await ( await fetch("https://stuff.leonardgeier.com/highscore?count=10") ).json();
+  const scores = await ( await fetch("https://stuff.leonardgeier.com/highscore?count=9999") ).json();
 
   const table_tag = document.getElementById("leaderboard-body");
   for (let i = 0; i < scores.length; i++) {
     const {name, score} = scores[i];
     const tag = document.createElement("tr");
-    tag.appendChild(create_td(i + 1));
+    tag.appendChild(create_td(String(i + 1) + "."));
     tag.appendChild(create_td(name));
     tag.appendChild(create_td(score));
     table_tag.appendChild(tag);
